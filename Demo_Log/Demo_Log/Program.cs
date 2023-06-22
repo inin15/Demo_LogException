c2da40339b8f020e9c3e6cacd776760628f0ba8c
inin15 <101159760+inin15@users.noreply.github.com>
2023-06-22 13:45:06 +0700

    Log Exception
    Demo_LogException

---
+++ Demo_Log/Demo_Log/Program.cs
@@ -0,0 +1,30 @@
+﻿using log4net;
+using System;
+
+namespace Demo_Log
+{
+    internal class Program
+    {
+        private static readonly ILog log =
+        LogManager.GetLogger(System.Reflection.MethodBase
+        .GetCurrentMethod().DeclaringType);
+
+        private static void Main(string[] args)
+        {
+            try {
+                
+                log.Info("Hello logging world!"); 
+                log.Info("Error");
+                Console.WriteLine("Hit enter");
+                Console.ReadLine();
+            }
+            catch(Exception e)
+            {
+                log.Info(e.ToString());
+            }
+            
+            
+            
+        }
+    }
+}
